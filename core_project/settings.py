d881a79adbc2ccf43e6efca9885e4fdb4f76568b
captmj <mrunaljadhav303@gmail.com>
2026-05-02 21:34:24 +0530

    Update settings for PythonAnywhere deployment

--- core_project/settings.py
+++ core_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)qaaj8!xm+!%(97hgbx%$#8aij%3rg40p5#a9s1p@2mr3a7lm&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
